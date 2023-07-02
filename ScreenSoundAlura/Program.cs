0ea1ea3af414c911b7db73d0dbcfbe29ed54c75a
Thainá Israel <thainaluizaisrael@gmail.com>
2023-07-02 16:16:30 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSoundAlura/Program.cs
@@ -0,0 +1,67 @@
+﻿// Screen Sound - Projeto Alura
+// Professor: Guilherme e Daniel Portugal
+// Desenvolvido por: Thainá Israel, 2023
+
+
+// C# é um alinguagem fortemente tipada, ou seja, é necessário declarar o tipo da variável(string, int, double, bool)
+// Nomes de variáveis são em camelCase, ou seja, a primeira letra da primeira palavra é minúscula e as demais palavras são maiúsculas
+// Quando uma função não retorna nada, ela é do tipo void
+// Nomes de função são em PascalCase, ou seja, a primeira letra de cada palavra é maiúscula
+string boasVindas = "Bem vindo(a) ao Screen Sound";
+void ExibirMensagemDeBoasVindas()
+{   // Verbatim string, ou seja, o texto é exibido exatamente como está escrito, sem precisar de caracteres de escape
+    Console.WriteLine(@"
+       
+    ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+    ██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+    ╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+    ░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+    ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+    ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+     ");
+    Console.WriteLine(boasVindas);
+
+}
+
+void ExbirOpcoesDoMenu()
+{
+    Console.WriteLine();
+    Console.WriteLine("1 - Registrar uma banda");
+    Console.WriteLine("2 - Listar bandas");
+    Console.WriteLine("3 - Avaliar banda");
+    Console.WriteLine("4 - Média de avaliação de uma banda");
+    Console.WriteLine("5 - Sair");
+    Console.WriteLine();
+    Console.Write("Escolha uma opção: ");
+
+    //Quando nao queremos trabalhar com valores nulos, podemos usar o operador !, que indica que a variável não é nula
+    string opcaoEscolha = Console.ReadLine()!;
+    int opcaoEscolhaNumerica = int.Parse(opcaoEscolha);
+
+    // Switch case é uma estrutura de decisão, que funciona como um if, mas é mais legível quando temos muitas opções
+    switch (opcaoEscolhaNumerica)
+    {
+        case 1:
+            Console.WriteLine("Registrar uma banda");
+            break;
+        case 2:
+            Console.WriteLine("Listar bandas");
+            break;
+        case 3:
+            Console.WriteLine("Avaliar banda");
+            break;
+        case 4:
+            Console.WriteLine("Média de avaliação de uma banda");
+            break;
+        case 5:
+            Console.WriteLine("Sair");
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+
+
+}
+ExibirMensagemDeBoasVindas();
+ExbirOpcoesDoMenu();
